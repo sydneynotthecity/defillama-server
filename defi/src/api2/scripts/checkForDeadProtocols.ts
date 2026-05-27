a6bd3a68a6a568d954cabed2a23df6889e5474f0
FelixBruguera <felixbruguera0@gmail.com>
2026-05-27 16:09:00 -0400

    Whitelist Credit from high borrowed alerts

--- defi/src/api2/scripts/checkForDeadProtocols.ts
+++ defi/src/api2/scripts/checkForDeadProtocols.ts
@@ -93,6 +93,7 @@ async function run() {
     'Starlay Finance', // latest astar tvl data was before marked insolvent
     'Fira', // backed by bUSD0 in 0xa428723eE8ffD87088C36121d72100B43F11fb6A (UZR lending market), but it is excluded from tvl to prevent doublecount with USD0
     'Milk Finance', // backed by unpriced MILK tokens that are intentionally excluded as it is the team's own token: 0x6E0090dBecF3b4F0F9429637756CaDD8Fc468C54
+    'Credit',
   ])
   const filteredHighBorrowedProtocols = verHighBorrowedProtocols.filter((i: any) => {
     return i.borrowedOrig  > 200_000 && !i.isMarkedDead && i.borrowedDiff > 5 && i.category === 'Lending' && !whitelistedSet.has(i.name)
